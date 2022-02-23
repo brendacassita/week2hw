68b021cc4efcfc91ed09eb1ac75fba8bf3e6addf
B <brendacassita@Brendas-MacBookPro.local>
2022-02-22 17:45:36 -0700

    showslist

---
+++ showslist/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ShowsList from './ShowsList';
+
+function App() {
+  return (
+    <div className="App">
+     <ShowsList />
+    </div>
+  );
+}
+
+export default App;
---
+++ showslist/src/ShowsList.js
@@ -0,0 +1,9 @@
+const ShowsList = (props) =>{
+  return(
+    <div>
+      <h1>Shows List</h1>
+    </div>
+  )
+}
+
+export default ShowsList
