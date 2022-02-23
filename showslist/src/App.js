cb2540eacd5e5eddc6969de1b7b44f3f7ede0327
B <brendacassita@Brendas-MacBookPro.local>
2022-02-22 18:43:59 -0700

    showslist

--- showslist/src/App.js
+++ showslist/src/App.js
@@ -5,7 +5,11 @@ import ShowsList from './ShowsList';
 function App() {
   return (
     <div className="App">
-     <ShowsList />
+     <ShowsList name='Shows to Watch List' initialShowsToWatch={[{id:1, watch: 'Ozark', seen:true}]} />
+     <ShowsList name='' initialShowsToWatch={[{id:1, watch: 'Breakiing Bad', seen:true}]} />
+     <ShowsList name='Animated Shows to Watch List' initialShowsToWatch={[{id:1, watch: 'Simpsons', seen:false}]} />
+     <ShowsList name='' initialShowsToWatch={[{id:1, watch: 'Futurama', seen:true}]} />
+     <ShowsList name='' initialShowsToWatch={[{id:1, watch: 'Disenchantment', seen:true}]} />
     </div>
   );
 }
--- showslist/src/ShowsList.js
+++ showslist/src/ShowsList.js
@@ -1,8 +1,27 @@
+import { useState } from "react"
+
 const ShowsList = (props) =>{
-  return(
+  const [watch, setWatch] = useState(props.initialShowsToWatch || [])
+  
+  // READ
+  const renderToWatch=()=>{
+    return watch.map((x)=>{
+      return (
+        <div key={x.id} style={{textDecoration: x.seen ? 'line-through': ''}}>
+        <p>{x.watch}</p>
+        </div>
+      )
+    })
+  }
+  
+  
+  return (
     <div>
-      <h1>Shows List</h1>
-    </div>
+      <h1>{props.name}</h1>
+
+      <div>{renderToWatch()}</div>
+     
+      </div>
   )
 }
 
